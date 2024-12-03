eb4c1638214fe6d63bf8f75292a6c769f8dcbfb7
readjfb <15671813+readjfb@users.noreply.github.com>
2024-12-02 23:24:46 -0600

    Advent of Code day 3- Proud of my solution, but I wish I had gone faster (I'm not great at regex). I'm happy that I didn't use any LLM or assistance beyond regex101.com to check my statements. Came in top 1000 for first part (~700), and 1400 for part 2. Code runs in 2 ms

---
+++ day3/day3.py
@@ -0,0 +1,34 @@
+import re
+
+
+def day3_pt1(puzzle_in):
+    s = 0
+    for row in puzzle_in:
+        for v in re.findall(r"mul\((\d+,\d+)\)", row):
+            i, j = v.split(",")
+            s += int(i) * int(j)
+    return s
+
+
+def day3_pt2(puzzle_in):
+    s = 0
+
+    active = True
+    for row in puzzle_in:
+        for p in re.findall(r"mul\((\d+,\d+)\)|(do)\(\)|(don't)\(\)", row):
+            if "don't" in p:
+                active = False
+            elif "do" in p:
+                active = True
+            elif active:
+                i, j = p[0].split(",")
+                s += int(i) * int(j)
+    return s
+
+
+if __name__ == "__main__":
+    with open("AdventOfCode-2024/day3/day3_input.txt") as file:
+        puzzle_in = [x.strip() for x in file.readlines()]
+
+    print(f"The solution to 3.1 is {day3_pt1(puzzle_in)}")
+    print(f"The solution to 3.2 is {day3_pt2(puzzle_in)}")
